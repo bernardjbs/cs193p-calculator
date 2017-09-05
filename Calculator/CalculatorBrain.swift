a3c5b3d44d0a402fd91ef07d417b7eb472d5bead
bernardjbs <bernardjbs@yahoo.com>
2017-09-06 07:23:13 +0800

    Add calculator brain model

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Bernard Sin Fat on 6/9/17.
+//  Copyright © 2017 Bernard Sin Fat. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+}
